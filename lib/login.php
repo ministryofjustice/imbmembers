5ec058a17385256bec8b6fdab5ef3c6c7d67e5af
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2023-12-05 11:43:37 +0000

    Allow access to wp-activate (when logged out) and style page

--- lib/login.php
+++ lib/login.php
@@ -111,5 +111,5 @@ function is_login_required()
  */
 function is_login_page()
 {
-    return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
+    return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php', 'wp-activate.php'));
 }
